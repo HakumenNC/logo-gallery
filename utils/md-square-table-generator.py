d09f9d7588271e96e1febd21a559ffb6a84f2ff8
Ronny SOUTART <soutartronny@gmail.com>
2025-04-26 08:49:14 +1100

    feat(mdSquareTableGenerator): add properties.yml files & process

--- utils/md-square-table-generator.py
+++ utils/md-square-table-generator.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import yaml
 
 OUTPUT_FILE = f"..{os.sep}README.md"
 IMG_PATH = f"..{os.sep}img"
@@ -12,10 +13,23 @@ RECTANGLE_30 = 'rectangle-1-30.png'
 def getItem(path):
     brandName = os.path.basename(path)
     basePath = path.replace(IMG_PATH, IMG_PATH_FROM_README) + os.sep
+    
     item = getBrandImg(basePath + SQUARE_60, brandName)
     if os.path.exists(path + os.sep + RECTANGLE_30):
         item += f' <br /> {getBrandImg(basePath + RECTANGLE_30, brandName)}'
-    return f'{item} <br /> `{brandName}`'
+
+    yamlProps = getYamlData(path)
+    if yamlProps is None:
+        return f'{item} <br /> `{brandName}`'
+    else:
+        return f"{item} <br /> `{brandName}` <br /> {yamlProps['label']} \| [website]({yamlProps['url']})"
+
+def getYamlData(path):
+    if os.path.exists(path + os.sep + 'properties.yml'):
+        with open(path + os.sep + 'properties.yml', 'r') as f:
+            return yaml.safe_load(f)
+    else: 
+        return None
 
 def getBrandImg(path, brandName):
     return f'![{brandName}]({winToPosixSep(path)} "{brandName}")'
